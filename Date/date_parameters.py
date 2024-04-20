3ae5c7206ddb4d910de8f47f4ca9c51f269ef81e
Anshul <anshagrawal2510@gmail.com>
2024-04-20 17:39:08 +0530

    refactor files

--- Date/date_parameters.py
+++ Date/date_parameters.py
@@ -1,6 +1,6 @@
-SPOT_PRICE = 22500
-EXPIRY_DATE = "04-Apr-2024"
-FILE_NAME = "Anshul"
+SPOT_PRICE = 22000
+EXPIRY_DATE = "26-sep-2024"
+FILE_NAME = "date"
 
 LIST_OF_PERCENTAGE = [5, 10, 20, 30, 40]
 FILE_FULL_PATH = "/Users/anshulagrawal/Downloads/" + FILE_NAME + ".csv"
